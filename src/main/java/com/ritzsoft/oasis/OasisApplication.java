d883d176c503988be6d67e9ad52127876c695837
jamesritala <jamesritala@gmail.com>
2018-12-17 17:51:54 +0200

    Initiaizing Project from Bootstrapping spring starter

---
+++ src/main/java/com/ritzsoft/oasis/OasisApplication.java
@@ -0,0 +1,14 @@
+package com.ritzsoft.oasis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OasisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OasisApplication.class, args);
+	}
+
+}
+
